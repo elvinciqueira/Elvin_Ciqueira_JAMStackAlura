d79e96d571345b673da9c03bd9634a591e049691
elvinciqueira <elvinciqueira@zsystems.com.br>
2021-02-11 18:35:15 -0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,21 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react'
+import {ThemeProvider} from 'styled-components'
+import Head from 'next/head'
+import GlobalStyle from '../src/theme/GlobalStyle'
+import theme from '../src/theme'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
-
-export default function App({ Component, pageProps }) {
+export default function App({Component, pageProps}) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
