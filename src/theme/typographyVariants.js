67e1762d747fc684c246d370643cd41756a5e826
elvinciqueira <elvinciqueira@gmail.com>
2021-02-13 16:41:07 -0300

    tamanho padrão da fonte ajustavel de acordo com a tela

--- src/theme/typographyVariants.js
+++ src/theme/typographyVariants.js
@@ -1,37 +1,37 @@
 const typographyVariants = {
   // [title]
   title: {
-    fontSize: '60px',
+    fontSize: '3.75rem',
     fontWeight: '400',
     fontFamily: 'Fira Sans Condensed, sans-serif',
     textTransform: 'uppercase',
   },
   xs: {
-    fontSize: '16px',
+    fontSize: '1rem',
     fontWeight: '400',
     fontFamily: 'Fira Sans Condensed, sans-serif',
   },
   // [subTitle]
   subTitle: {
-    fontSize: '48px',
+    fontSize: '3rem',
     fontWeight: '300',
     fontFamily: 'Fira Sans Condensed, sans-serif',
   },
   // [subTitle2]
   subTitle2: {
-    fontSize: '32px',
+    fontSize: '2rem',
     fontWeight: '700',
     fontFamily: 'Fira Sans Condensed, sans-serif',
   },
   // [paragraph1]
   paragraph1: {
-    fontSize: '28px',
+    fontSize: '1.75rem',
     fontWeight: '400',
     fontFamily: 'Fira Sans Condensed, sans-serif',
   },
   // [bodyText]
   bodyText: {
-    fontSize: '18px',
+    fontSize: '1.125rem',
     fontWeight: '400',
     lineHeight: 1.5,
     fontFamily: 'Fira Sans, sans-serif',
