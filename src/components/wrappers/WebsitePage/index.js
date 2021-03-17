4b6f850265289cbb20649482e1ef3f4fc1efb717
elvinciqueira <elvinciqueira@gmail.com>
2021-03-17 14:56:16 -0300

    feat: componente SEO

--- src/components/wrappers/WebsitePage/index.js
+++ src/components/wrappers/WebsitePage/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-pascal-case */
 import React from 'react'
 import PropTypes from 'prop-types'
 import Footer from '../../Footer'
@@ -7,37 +8,21 @@ import Modal from '../../common/Modal'
 import FormContato from '../../patterns/FormContato'
 import {Box} from '../../foundation/layout/Box'
 import SectionDivider from '../../common/SectionDivider'
-
-WebsitePageWrapper.defaultProps = {
-  pageBoxProps: {},
-  capaProps: {
-    display: true,
-  },
-}
-
-WebsitePageWrapper.propTypes = {
-  capaProps: PropTypes.shape({
-    display: PropTypes.bool,
-  }),
-  pageBoxProps: PropTypes.shape({
-    backgroundImage: PropTypes.string,
-    backgroundRepeat: PropTypes.string,
-    backgroundPosition: PropTypes.string,
-  }),
-  children: PropTypes.node.isRequired,
-}
+import SEO from '../../common/SEO'
 
 const WebsitePageContext = React.createContext({
   toggleModal: () => {},
 })
 
-function WebsitePageWrapper({children, capaProps, pageBoxProps}) {
+function WebsitePageWrapper({children, capaProps, pageBoxProps, seoProps}) {
   const [isModalOpen, setModalState] = React.useState(false)
 
   const toggleModal = () => setModalState(!isModalOpen)
 
   return (
     <WebsitePageContext.Provider value={{toggleModal}}>
+      <SEO {...seoProps} />
+
       {capaProps.display && <Capa />}
 
       <Box {...pageBoxProps}>
@@ -63,6 +48,29 @@ function useWebsitePageContext() {
   const context = React.useContext(WebsitePageContext)
 
   return context
+}
+
+WebsitePageWrapper.defaultProps = {
+  pageBoxProps: {},
+  seoProps: {},
+  capaProps: {
+    display: true,
+  },
+}
+
+WebsitePageWrapper.propTypes = {
+  capaProps: PropTypes.shape({
+    display: PropTypes.bool,
+  }),
+  seoProps: PropTypes.shape({
+    headTitle: PropTypes.string,
+  }),
+  pageBoxProps: PropTypes.shape({
+    backgroundImage: PropTypes.string,
+    backgroundRepeat: PropTypes.string,
+    backgroundPosition: PropTypes.string,
+  }),
+  children: PropTypes.node.isRequired,
 }
 
 export {WebsitePageWrapper, useWebsitePageContext}
