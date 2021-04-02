c1cd6698eb9248c59ca9a006f67a68a76f764040
elvinciqueira <elvinciqueira@gmail.com>
2021-04-02 18:12:50 -0300

    feat: testando componente Typography

--- src/components/foundation/Typography/Typography.spec.js
+++ src/components/foundation/Typography/Typography.spec.js
@@ -4,6 +4,18 @@ import Typography from './index'
 
 describe('<Typography />', () => {
   describe('when component renders', () => {
+    test('it applies default styles', () => {
+      const tree = rendererCreateWithTheme(<Typography />).toJSON()
+
+      expect(tree).toMatchSnapshot()
+      expect(tree).toHaveStyleRule('font-size', '1.75rem')
+      expect(tree).toHaveStyleRule('font-weight', '400')
+      expect(tree).toHaveStyleRule(
+        'font-family',
+        'Fira Sans Condensed,sans-serif',
+      )
+    })
+
     test('textAlign props sets text-align', () => {
       const tree = rendererCreateWithTheme(
         <Typography textAlign="center" />,
@@ -11,6 +23,24 @@ describe('<Typography />', () => {
 
       expect(tree).toMatchSnapshot()
       expect(tree).toHaveStyleRule('text-align', 'center')
+    })
+
+    test('margin props sets margin', () => {
+      const tree = rendererCreateWithTheme(
+        <Typography margin="20px" />,
+      ).toJSON()
+
+      expect(tree).toMatchSnapshot()
+      expect(tree).toHaveStyleRule('margin', '20px')
+    })
+
+    test('fontSize props sets font-size', () => {
+      const tree = rendererCreateWithTheme(
+        <Typography fontSize="1rem" />,
+      ).toJSON()
+
+      expect(tree).toMatchSnapshot()
+      expect(tree).toHaveStyleRule('font-size', '1rem')
     })
   })
 })
