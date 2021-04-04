cf67fa4d5b4714d59a1386a6ffb827b2a7477419
elvinciqueira <elvinciqueira@gmail.com>
2021-04-04 15:44:32 -0300

    test contact service

--- src/services/contact/contactService.spec.js
+++ src/services/contact/contactService.spec.js
@@ -1,30 +1,40 @@
+import HttpClientModule from '../../infra/HttpClient'
 import {contactService} from './contactService'
 
-const body = {
-  name: 'John Doe',
-  email: 'johndoe@example.com',
-  message: 'somemessage',
-}
-
-function HttpClientModule() {
-  return {
-    data: {
-      ...body,
-    },
-  }
-}
+jest.mock('../../infra/HttpClient')
 
 describe('contactService', () => {
   describe('execute()', () => {
     describe('when user try to contact', () => {
       describe('and succeed', () => {
         test('return response values properly', async () => {
+          const url = 'https://contact-form-api-jamstack.herokuapp.com/message'
+          const body = {
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            message: 'somemessage',
+          }
+          const requestParameters = {
+            body,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+          }
+
+          HttpClientModule.mockReturnValueOnce(() => ({
+            ok: 200,
+            data: {...body},
+          }))
+
           const contactServiceResponse = await contactService.execute(
             body,
             HttpClientModule,
           )
 
-          expect(contactServiceResponse.data).toEqual(body)
+          expect(contactServiceResponse().data).toEqual(body)
+          expect(HttpClientModule).toHaveBeenCalledTimes(1)
+          expect(HttpClientModule).toHaveBeenCalledWith(url, requestParameters)
         })
       })
     })
